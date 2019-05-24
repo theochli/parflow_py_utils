4b4cea4f6ace3c39b8c96c1ba9a6a97a64f917b9
theo <chaolim85@gmail.com>
2019-05-24 14:55:38 -0400

    rm test and ref files
    - move subsurface pre-proc f2py scripts to the top subsurface
    directory

--- subsurface/my_test/pfbtosilo.tcl
+++
@@ -1,5 +0,0 @@
-lappend auto_path $env(PARFLOW_DIR)/bin 
-package require parflow
-namespace import Parflow::*
-
-pfsave [pfload "permeability.pfb"] -silo "permeability.silo"
--- subsurface/test/README.md
+++
@@ -1,1 +0,0 @@
-https://docs.scipy.org/doc/numpy-1.10.0/user/c-info.python-as-glue.html
