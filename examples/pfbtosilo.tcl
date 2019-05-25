39947ca3786d423a5eda17c37259ddb73803e4f9
Theo Lim <tclim@vt.edu>
2019-05-24 22:31:42 -0400

    made heterogeneous subsurface pfb files for run - perm, indicator, and mannings.

--- examples/pfbtosilo.tcl
+++ examples/pfbtosilo.tcl
@@ -2,7 +2,8 @@ lappend auto_path $env(PARFLOW_DIR)/bin
 package require parflow
 namespace import Parflow::*
 
-pfsave [pfload "testperm.pfb"] -silo "testperm.silo"
-#pfsave [pfload "testmann.pfb"] -silo "testmann.silo"
+pfsave [pfload "perm_het1.pfb"] -silo "perm_het1.silo"
+pfsave [pfload "mann_het1.pfb"] -silo "mann_het1.silo"
+pfsave [pfload "ind_het1.pfb"] -silo "ind_het1.silo"
 
 
