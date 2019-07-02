148ebcef82b07783cf47e3648cf72f784a803ac9
Theo Lim <tclim@vt.edu>
2019-07-02 14:31:23 -0400

    added separate directory with files needed to monitor spinup

---
+++ vdz_wb/silotopfb_iter_bnam.tcl
@@ -0,0 +1,18 @@
+
+
+lappend auto_path $env(PARFLOW_DIR)/bin
+package require parflow
+namespace import Parflow::*
+
+
+set tcl_precision 16
+
+set fbasenam [lindex $argv 0]
+set start [lindex $argv 1]
+set end [lindex $argv 2]
+
+for {set i $start } {$i <= $end} {incr i 1} {
+	set silo [pfload $fbasenam.$i.silo]
+	pfsave $silo -pfb [format "$fbasenam.$i.pfb"]
+
+}
