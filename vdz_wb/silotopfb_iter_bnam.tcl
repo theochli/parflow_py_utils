64418b8b42cf703a49f33cdbb17f85597debfe59
theo <tclim@vt.edu>
2019-07-03 22:05:58 +0000

    changed silotopfb to take file databases with fixed with index numbers in file name (evaptranssum) vdz water balance steps all in one jupyter notebook, includng: - surface, (vdz) subsurface storage - overland flow, (vdz) evapotranspiration - loading in nldas data
    Something funny happening with the water balance

--- vdz_wb/silotopfb_iter_bnam.tcl
+++ vdz_wb/silotopfb_iter_bnam.tcl
@@ -10,9 +10,21 @@ set tcl_precision 16
 set fbasenam [lindex $argv 0]
 set start [lindex $argv 1]
 set end [lindex $argv 2]
+set fw [lindex $argv 3]
 
 for {set i $start } {$i <= $end} {incr i 1} {
+	if $fw {
+	
+	set filename [format "%s.%05d.silo" $fbasenam $i]
+	set silo [pfload $filename]
+	pfsave $silo -pfb [format "$fbasenam.$i.pfb"]
+
+        } {
+	
 	set silo [pfload $fbasenam.$i.silo]
 	pfsave $silo -pfb [format "$fbasenam.$i.pfb"]
+        
+        }
+	
 
 }
